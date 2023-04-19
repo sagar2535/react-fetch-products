6b392d75bdfa323bc753fe2d753edf41f2fe7fbf
sagar-kumar <sagarkumar2535@gmail.com>
2023-04-20 00:41:13 +0530

    react-fetch-products

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const fetchProducts = async () => {
+    const res = await fetch('https://dummyjson.com/products');
+    const data = await res.json();
+    if (data && data.products) {
+      console.log(data);
+      setProducts(data.products);
+    }
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {products.length > 0 && (
+        <div className='products'>
+          {products.map((prod) => (
+            <span key={prod.id} className='products__single'>
+              <img
+                style={{ cursor: 'pointer' }}
+                src={prod.thumbnail}
+                alt={prod.title}
+              />
+              <span>{prod.title}</span>
+            </span>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
+/*
+ fetch('https://dummyjson.com/products')
+.then(res => res.json())
+.then(console.log);
+ */
